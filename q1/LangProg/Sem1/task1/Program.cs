a2b5caea284cfeb15ee89f905189af9b1cdf0fce
Dmitry Kabanov <kabanov_d@bk.ru>
2023-02-12 00:26:14 +0600

    task1_sem1

---
+++ q1/LangProg/Sem1/task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Inter number 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Inter number 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1/number2 == number2){
+    Console.Write($"Число {number1} является квадратом чиста {number2}");
+} else
+{
+    Console.Write($"Число {number1} не является квадратом числа {number2}");
+}
+
+
