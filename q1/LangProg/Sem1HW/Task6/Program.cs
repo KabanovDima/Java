2c971e28db69e21139874738b1f63a1fb3c0d18a
Dmitry Kabanov <kabanov_d@bk.ru>
2023-02-12 22:34:02 +0600

    done HW task#6

---
+++ q1/LangProg/Sem1HW/Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Type number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ($"You typed number: {number} ");
+
+int numberEven = number % 2;
+
+if (numberEven == 0) {
+    Console.Write($"number {number} is: even");
+} else {
+    Console.Write($"number {number} is: not even");
+}
