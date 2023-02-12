861e571658a7f970ff6e370ec3587edbaefde6b8
Dmitry Kabanov <kabanov_d@bk.ru>
2023-02-12 22:47:35 +0600

    done HW task#8

---
+++ q1/LangProg/Sem1HW/Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write ("Type number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ($"You typed number: {number} ");
+
+int x = 1; 
+
+while (x <= number)
+{
+    if((x % 2) == 0){
+    Console.Write($"{x} ");
+    }
+    x++;
+}
+
+
