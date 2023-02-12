f8dca6411d274edd0ddb4c8a4bdeb7d942736940
Dmitry Kabanov <kabanov_d@bk.ru>
2023-02-12 22:21:15 +0600

    done HW task#4

---
+++ q1/LangProg/Sem1HW/Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write ("Type number 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Type number 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Type number 3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ($"You typed two numbers: {number1}, {number2}, {number3}.");
+
+int MAX = 0;
+
+if(number1 > number2) {
+    MAX = number1;
+} else {
+    MAX = number2;
+}
+if (number3 > MAX){
+    MAX = number3;
+}
+Console.Write($"MAX number is: {MAX}");
