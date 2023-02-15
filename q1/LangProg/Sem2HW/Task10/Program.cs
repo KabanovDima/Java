cfd39941ccd2c79c6a46b3aa90760a2d97ecea46
Dmitry Kabanov <kabanov_d@bk.ru>
2023-02-15 23:11:55 +0600

    Did Task10 Sem#2

---
+++ q1/LangProg/Sem2HW/Task10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.WriteLine("Enter a three-digit number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 99 && number < 1000){
+
+int first2Digit = number/10;
+int digit2 = first2Digit%10;
+
+System.Console.WriteLine(digit2);
+} else {
+    System.Console.WriteLine("You type not a three-digit number");
+}
