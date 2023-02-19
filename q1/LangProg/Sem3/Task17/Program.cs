66718af616264cfbca85015e7cfcec4aca71f344
Dmitry Kabanov <kabanov_d@bk.ru>
2023-02-19 22:49:56 +0600

    Did Task15 Sem#2

---
+++ q1/LangProg/Sem3/Task17/Program.cs
@@ -0,0 +1,30 @@
+﻿// // Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y),
+// // причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+int PromtStringtoInt (string message) {
+    System.Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+string Quaters (int x, int y){
+    string result = "";
+    if (x > 0 && y > 0){
+        result = "Point lies in the first quarter";
+    } else if (x < 0 && y > 0){
+        result = "Point lies in the second quarter";
+    } else if (x < 0 && y < 0){
+        result = "Point lies in the third quarter";
+    } else if (x > 0 && y < 0){
+        result = "Point lies in the fourth quarter";
+    } else {
+        result = "Point lies on one of the axes";
+    }
+    return result;
+}
+
+
+int X = PromtStringtoInt("Enter X coordinate: ");
+int Y = PromtStringtoInt("Enter Y coordinate: ");
+string quarter = Quaters (X, Y);
+System.Console.WriteLine(quarter);
