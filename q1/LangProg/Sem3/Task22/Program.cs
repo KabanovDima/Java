1073c97a71810dfe0ada8bde634ae6f21a8dbcb2
Dmitry Kabanov <kabanov_d@bk.ru>
2023-02-20 22:43:16 +0600

    Did Task22 Sem#3

---
+++ q1/LangProg/Sem3/Task22/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел
+// от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+// Var1:
+// System.Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int x = 1;
+// int y = 0;
+
+// while (x <= number)
+// {
+//     y = x * x;
+//     System.Console.Write($"{y} ");
+//     x++;
+// }
+
+//Var2:
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 1; i <= number; i++){
+    System.Console.Write($"{Math.Pow(i, 2)} ");
+}
