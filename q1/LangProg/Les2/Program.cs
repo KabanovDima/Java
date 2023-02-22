6ab0de59099ae513da4e5bdfcce153ff06f50c32
Dmitry Kabanov <kabanov_d@bk.ru>
2023-02-22 23:31:52 +0600

    Lesson2 Introduction to functions and arrays

---
+++ q1/LangProg/Les2/Program.cs
@@ -0,0 +1,40 @@
+﻿void FillArray(int[] arr){
+    int length = arr.Length;
+    int index = 0;
+    while(index < length){
+        arr[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] array){
+    int length = array.Length;
+    int index = 0;
+    while(index < length) {
+        System.Console.Write($"{array[index]} ");
+        index++;
+    }
+}
+
+int IndexOf(int[] array, int find){
+    int length = array.Length;
+    int index = 0;
+    int position = -1;
+
+    while(index < length){
+        if(array[index] == find){
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] arr = new int[10];
+
+FillArray(arr);
+PrintArray(arr);
+System.Console.WriteLine();
+int pos = IndexOf(arr, 4);
+System.Console.WriteLine(pos);
