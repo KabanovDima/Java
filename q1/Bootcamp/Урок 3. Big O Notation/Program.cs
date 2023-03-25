cb800f3cd4b7a15921ddfb0f820d163003e09758
Dmitry Kabanov <kabanov_d@bk.ru>
2023-03-25 14:49:35 +0600

    Added bootcamp les 2 and 3

---
+++ q1/Bootcamp/Урок 3. Big O Notation/Program.cs
@@ -0,0 +1,27 @@
+﻿// VAR1
+// int n = Convert.ToInt32(Console.ReadLine());
+// for (int i = 1; i <= n; i++){
+//    for (int j = 1; j <= n; j++){
+//     System.Console.Write(i*j);
+//     System.Console.Write("\t");
+//    } 
+//    System.Console.WriteLine();
+// }
+
+// VAR2
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[n,n];
+
+for (int i = 0; i < n; i++){
+    for (int j = i; j < n; j++){
+        array[i,j] = (i+1)*(j+1);
+        array[j,i] = (i+1)*(j+1);
+    }
+}
+for (int i = 0; i < n; i++){
+    for (int j = 0; j < n; j++){
+        System.Console.Write(array[i,j]);
+        System.Console.Write("\t");
+    }
+    System.Console.WriteLine();
+}
